d93bce6314c6a1f06b96fcde7709bd63e9f776ba
akhmadiy <ahmadulinjs@gmail.com>
2022-03-15 14:07:21 +0500

    chore: disable example code

--- service.js
+++ service.js
@@ -1,35 +1,34 @@
-class Service {
-    constructor(options) {
-      this.transport = options.transport;
-      this.isClusterMode = !!options.cluster;
-      if (this.isClusterMode) {
-        this.clusterOptions = options.cluster;
-      }
-    }
-  
-    async start() {
-      if (this.isClusterMode) {
-        if (this.isMaster) {
-          await this.startCluster();
-          if (this.transport.isPermanentConnection) {
-            await this.startTransport();
-          }
-        } else {
-          await this.startWorker();
-          if (!this.transport.isPermanentConnection) {
-            await this.startTransport();
-          }
-        }
-      } else {
-        await this.startWorker();
-        await this.startTransport();
-      }
-    }
-  
-    async startTransport() {}
-  
-    async startWorker() {}
-  
-    async startCluster() {}
-  }
-  
+// class Service {
+//   constructor(options) {
+//     this.transport = options.transport;
+//     this.isClusterMode = !!options.cluster;
+//     if (this.isClusterMode) {
+//       this.clusterOptions = options.cluster;
+//     }
+//   }
+
+//   async start() {
+//     if (this.isClusterMode) {
+//       if (this.isMaster) {
+//         await this.startCluster();
+//         if (this.transport.isPermanentConnection) {
+//           await this.startTransport();
+//         }
+//       } else {
+//         await this.startWorker();
+//         if (!this.transport.isPermanentConnection) {
+//           await this.startTransport();
+//         }
+//       }
+//     } else {
+//       await this.startWorker();
+//       await this.startTransport();
+//     }
+//   }
+
+//   async startTransport() {}
+
+//   async startWorker() {}
+
+//   async startCluster() {}
+// }
