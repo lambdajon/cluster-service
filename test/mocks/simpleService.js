e5d9e3c7b391f653bc8964dd28feb3d4ac349e87
akhmadiy <ahmadulinjs@gmail.com>
2022-03-16 18:40:15 +0500

    refactor: change interface service

--- test/mocks/simpleService.js
+++ test/mocks/simpleService.js
@@ -1,7 +1,11 @@
 const Service = require('../../src/service');
-const HttpTransport = require('../../src/transport/transport');
 
-const service = new Service({ transport: new HttpTransport(3000) });
+const service = new Service({
+  transport: {
+    type: 'http',
+    port: 3000
+  }
+});
 
 (async () => {
   await service.start();
